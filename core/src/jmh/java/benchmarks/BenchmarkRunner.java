1053ee3464a125c87f12bc6886923c8c5e3cb06f
cklellie <christopherjkelly@gmail.com>
2020-01-06 14:32:54 +0100

    Tested

--- core/src/jmh/java/benchmarks/BenchmarkRunner.java
+++ core/src/jmh/java/benchmarks/BenchmarkRunner.java
@@ -4,5 +4,12 @@
  * This software is licensed under under GPL-3.0-only or GPL-3.0-or-later, https://opensource.org/licenses/GPL-3.0
  */
 
-//-*- coding: utf-8 -*-
-include 'core'
+package benchmarks;
+
+public class BenchmarkRunner
+{
+    public static void main( String[] args ) throws Exception
+    {
+        org.openjdk.jmh.Main.main( args );
+    }
+}
